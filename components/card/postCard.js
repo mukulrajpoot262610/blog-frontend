bf0de186947754b20ced90e11a4fb532bed98327
Amit Kumar <alsoamit@gmail.com>
2022-09-04 12:27:47 +0530

    feat: show likes in PostCard

--- components/card/postCard.js
+++ components/card/postCard.js
@@ -41,21 +41,7 @@ const PostCard = ({ data }) => {
             </svg>
           </a>
           <span className="inline-flex items-center gap-1 py-1 pr-3 ml-auto mr-3 text-sm leading-none text-gray-400 border-r-2 border-gray-200">
-            <FiHeart /> 1.2K
-          </span>
-          <span className="inline-flex items-center text-sm leading-none text-gray-400">
-            <svg
-              className="w-4 h-4 mr-1"
-              stroke="currentColor"
-              strokeWidth="2"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              viewBox="0 0 24 24"
-            >
-              <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-            </svg>
-            6
+            <FiHeart /> {data?.likes || '0'}
           </span>
         </div>
         <a className="inline-flex items-center">
@@ -66,7 +52,7 @@ const PostCard = ({ data }) => {
           />
           <span className="flex flex-col flex-grow pl-4">
             <span className="font-medium text-gray-900 title-font">
-              {data?.author?.name || 'Author'}
+              {data?.author?.name || 'You'}
             </span>
             <span className="text-gray-400 text-xs tracking-widest mt-0.5">
               {formatDistance(new Date(Date.now()), new Date(data?.createdAt))}
